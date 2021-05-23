5d586a0ef9558edc0b80b3af46adba16c7732ab3
mrjesse1283 <mrjesse1283@gmail.com>
2021-05-23 16:09:36 -0600

    rtv part4/5

--- week6/part_4/client/src/App.js
+++ week6/part_4/client/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext } from 'react'
+import { Switch, Route, Redirect } from 'react-router-dom'
+import Navbar from './components/Navbar.js'
+import Auth from './components/Auth.js'
+import Profile from './components/Profile.js'
+import Public from './components/Public.js'
+import { UserContext } from './context/UserProvider.js'
+import ProtectedRoute from './components/ProtectedRoute'
 
-function App() {
+export default function App(){
+  const { token, logout } = useContext(UserContext)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      { token && <Navbar logout={logout}/>}
+      <Switch>
+        <Route 
+          exact path="/" 
+          render={()=> 
+            token ? 
+            <Redirect to="/profile"/> 
+            : <Auth />}
+        />
+        <ProtectedRoute 
+          path="/profile"
+          component={ Profile }
+          redirectTo='/'
+          token= { token }
+          // render={() =>
+          //   !token ? 
+          //   <Redirect to ="/"/>
+          //   : <Profile/>}
+        />
+        <ProtectedRoute 
+          path="/public"
+          component={ Public}
+          redirectTo='/'
+          token= { token }
+          // render={() => 
+          // !token ? 
+          // <Redirect to = "/"/>
+          // : <Public />}
+        />
+      </Switch>
     </div>
-  );
+  )
 }
-
-export default App;
